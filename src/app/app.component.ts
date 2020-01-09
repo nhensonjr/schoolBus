8d5e5db5ea40aa365cc5fa69165f76453c899a6d
Norman Henson Jr <nhensonjr@gmail.com>
2020-01-09 00:28:00 -0600

    initial commit rough draft

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,82 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {FormControl} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   template: `
-    <!--The content below is only a placeholder and can be replaced.-->
-    <div style="text-align:center" class="content">
-      <h1>
-        Welcome to {{title}}!
-      </h1>
-      <span style="display: block">{{ title }} app is running!</span>
-      <img width="300" alt="Angular Logo" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICAgIDxwYXRoIGZpbGw9IiNERDAwMzEiIGQ9Ik0xMjUgMzBMMzEuOSA2My4ybDE0LjIgMTIzLjFMMTI1IDIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMXoiIC8+CiAgICA8cGF0aCBmaWxsPSIjQzMwMDJGIiBkPSJNMTI1IDMwdjIyLjItLjFWMjMwbDc4LjktNDMuNyAxNC4yLTEyMy4xTDEyNSAzMHoiIC8+CiAgICA8cGF0aCAgZmlsbD0iI0ZGRkZGRiIgZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiAvPgogIDwvc3ZnPg==">
+    <div class="header">
+      <div>{{title}}</div>
+      <div class="header-buttons">
+        <div>
+          <i (click)="toggleAddMoney()" class="fa fa-plus settings"></i>
+        </div>
+        <div>
+          <i (click)="toggleSettings()" class="fa fa-gears settings"></i>
+        </div>
+      </div>
     </div>
-    <h2>Here are some links to help you start: </h2>
-    <ul>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/tutorial">Tour of Heroes</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/cli">CLI Documentation</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://blog.angular.io/">Angular blog</a></h2>
-      </li>
-    </ul>
-    
+
+    <div class="body">
+      <div class="saving-target">
+        <h1>Savings Goal: {{"$" + savingsGoal}}</h1>
+        <h1></h1>
+      </div>
+
+      <div class="image-slider">
+        <div class="fill" [style.width.%]="percentageSaved">
+          <img src="../assets/filledBus.png"/>
+        </div>
+        <img src="../assets/emptyBus.png"/>
+      </div>
+
+      <div class="add-money-container">
+        <div (click)="totalUpMoney()" class="w3-green w3-hover-grey w3-margin-right add-money-button">ADD MONEY</div>
+        <input class="w3-input" type="text" [formControl]="fundsToAdd">
+      </div>
+
+      <div [ngClass]="showSettings ? 'w3-modal show' : 'hide'">
+        <div class="w3-modal-content w3-card-4">
+          <header class="modal-header w3-green">
+            <div class="w3-container"><h3>Settings</h3></div>
+            <div (click)="toggleSettings()" class="w3-container settings">X</div>
+          </header>
+          <div class="w3-container">
+            <p>Some text..</p>
+            <p>Some text..</p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="footer">
+      FOOTER
+    </div>
   `,
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'schoolBus';
+  title = 'Bus Fund';
+  fundsToAdd = new FormControl(0.00);
+  totalSaved = 0.00;
+  savingsGoal = 10000.00;
+  showSettings = false;
+  addMoney = false;
+
+  get percentageSaved() {
+    return ((this.totalSaved / this.savingsGoal) * 100);
+  }
+
+  toggleSettings() {
+    this.showSettings = !this.showSettings;
+  }
+
+  toggleAddMoney() {
+    this.addMoney = !this.addMoney;
+  }
+
+  totalUpMoney() {
+    console.log(this.totalSaved);
+    this.totalSaved += this.fundsToAdd.value;
+    console.log(this.totalSaved);
+  }
 }
